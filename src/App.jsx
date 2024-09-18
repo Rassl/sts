9197ad56765fcdc84043566333e941627ba9f611
Расул <rasul@MacBook-Air.local>
2024-09-18 22:17:38 +0300

    feat: add cubes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Canvas } from "@react-three/fiber";
 import { Experience } from "./components/Experience";
+import { Cubes } from "./components/Cubes";
 
 function App() {
   return (
     <Canvas shadows camera={{ position: [3, 3, 3], fov: 30 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
+      <Cubes />
     </Canvas>
   );
 }
